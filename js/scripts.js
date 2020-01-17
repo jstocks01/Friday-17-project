15e2392d5cdecfafc1b893928812eda67ef90b96
Jason Stocks <dicipleofrollins@gmail.com>
2020-01-17 10:38:23 -0800

    add image

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,55 @@
 $(document).ready(function(){
-event.preventDefault();
+
+  $("#submit").click(function(){
+
+    event.preventDefault();
+
+
+
+
+
+    var Language = $("input:radio[name=Language]:checked").val();
+
+
+
+    if (Language === "x"){
+
+      alert("You have selected Javascript")
+
+      $("#Javascript").show();
+
+      $("#Ruby").hide();
+
+      $("#Python").hide();
+
+    } else if (Language === "y"){
+
+      alert("You have selected Ruby")
+
+      $("#Ruby").show();
+
+      $("#Javascript").hide();
+
+      $("#Python").hide();
+
+    } else {
+
+      alert("You have selected Python")
+
+      $("#Python").show();
+
+      $("#Javascript").hide();
+
+      $("#Ruby").hide();
+
+    }
+
+
+
+
+
+  })
+
+  event.preventDefault();
+
+})
