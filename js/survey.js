f085486a298b4ce04e09e310373bd5aea878f6d7
Jason Stocks <dicipleofrollins@gmail.com>
2020-01-17 13:52:34 -0800

    update html

---
+++ js/survey.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+
+
+  $("#submitsurvey").click(function(){
+
+
+
+    event.preventDefault();
+
+
+
+
+
+
+
+
+
+
+
+    var beverage = parseInt($("#beverage").val());
+    var starwars = parseInt($("#starwars").val());
+    var travel = parseInt($("#travel").val());
+    var food = parseInt($("#food").val());
+    var sushi = parseInt($("#sushi").val());
+
+    
+var total = beverage + starwars + travel + food + sushi;
+
+
+
+if (total < 7 ){
+alert("you should choose Ruby!");}
+
+else if (total > 6 && total <= 10 ){
+alert("you should choose C#!");}
+
+else if (total > 10 ){
+ alert("you should choose python!");}
+
+ }
+
+
+
+
+
+
+
+
+
+
+)
+
+ event.preventDefault();
+
+
+
+})
