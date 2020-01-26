54687e72b37c7d5f5b3f9fc70a02d8c38300a3ed
jason <dicipleofrollins@gmail.com>
2020-01-26 15:16:36 -0800

    add refactoring changes spacing changes removal of alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,18 @@
 $(document).ready(function(){
-
   $("#submit").click(function(){
-
-    event.preventDefault();
-
     var Language = $("input:radio[name=Language]:checked").val();
-
     if (Language === "x"){
-
-      alert("You have selected Javascript")
-
       $("#Javascript").show();
-
       $("#Ruby").hide();
-
       $("#Python").hide();
-
-
-
     } else if (Language === "y"){
-
-      alert("You have selected Ruby")
-
       $("#Ruby").show();
-
       $("#Javascript").hide();
-
       $("#Python").hide();
-
-
     } else {
-
-      alert("You have selected Python")
-
       $("#Python").show();
-
       $("#Javascript").hide();
-
       $("#Ruby").hide();
-
     }
-
-
-
-
-
-
-
-
-
-
-
   })
-
-
-
-  event.preventDefault();
-
-
-
 })
--- js/survey.js
+++ js/survey.js
@@ -1,30 +1,25 @@
 $(document).ready(function(){
-
-$("#submitsurvey").click(function(){
-
-event.preventDefault();
-
-var beverage = parseInt($("#beverage").val());
-var starwars = parseInt($("#starwars").val());
-var travel = parseInt($("#travel").val());
-var food = parseInt($("#food").val());
-var sushi = parseInt($("#sushi").val());
-
-    
-var total = beverage + starwars + travel + food + sushi;
-
-
-if (total < 7 ){
-alert("you should choose Ruby!");}
-
-else if (total > 6 && total <= 10 ){
-alert("you should choose C#!");}
-
-else if (total > 10 ){
- alert("you should choose python!");}
- 
- }
- 
-)
- event.preventDefault();
+  $("#submitsurvey").click(function(){
+    var beverage = parseInt($("#beverage").val());
+    var starwars = parseInt($("#starwars").val());
+    var travel = parseInt($("#travel").val());
+    var food = parseInt($("#food").val());
+    var sushi = parseInt($("#sushi").val());    
+    var total = beverage + starwars + travel + food + sushi;
+    if (total < 7 ){
+      $("#Ruby").show();
+      $("#Javascript").hide();
+      $("#Python").hide();
+    }
+    else if (total > 6 && total <= 10 ){
+      $("#Javascript").show();
+      $("#Ruby").hide();
+      $("#Python").hide();
+    }
+    else {
+      $("#Python").show();
+      $("#Javascript").hide();
+      $("#Ruby").hide();
+    }
+  })
 })
